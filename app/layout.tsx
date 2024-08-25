5ebc13754845a20fc04bd53ea911d7c6eccf2ee9
V4l3ntino <valentinohontar15@gmail.com>
2024-08-25 16:56:50 +0200

    pagina de presentación con botones de neon

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Urbanist } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
+import Header from "@/components/header";
 
 const urbanist = Urbanist({ subsets: ["latin"] });
 
@@ -16,9 +17,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="es">
       <body className={urbanist.className}>
         <Navbar />
+        <Header />
         {children}
         </body>
     </html>
