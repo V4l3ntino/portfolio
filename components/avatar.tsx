17f1c7cdce5b0f2dbf7d5400e05c4e8b46728284
Valentino <valentinohontar15@gmail.com>
2024-08-26 18:01:25 +0200

    Pagina services con slider

--- components/avatar.tsx
+++ components/avatar.tsx
@@ -1,10 +1,12 @@
+"use client"
+
 import Image from "next/image";
 import MotionTransition from "./transition-component";
 
 const Avatar = () => {
     return ( 
         <MotionTransition position="bottom" className="bottom-0, right-0 hidden md:inline-block md:absolute">
-            <Image src="/avatar-1.png" width={400} height={400}
+            <Image src="/avatar-1.png" width={350} height={350}
             className="w-full h-full" alt="Avatar" />
         </MotionTransition>
      );
